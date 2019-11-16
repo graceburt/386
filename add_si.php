1db9075e43588b5985176e9860fa094488661121
mattblairro <mrovine1@acadweb1.salisbury.edu>
2019-11-16 17:20:38 -0500

    added the add_si first draft

---
+++ add_si.php
@@ -0,0 +1,61 @@
+<header>
+
+<nav id="navigation">
+  <ul class="links" style = "float:left;">
+  <li><a href="#">Home</a></li>
+    <li class="dropdown"><a href="#" class="trigger-drop">Edit SI<i class="arrow"></i></a>
+      <ul class="drop">
+        <li><a href="#">Add</a></li>
+        <li><a href="#">Delete</a></li>
+        <li><a href="#">Update</a></li>
+      </ul>
+    </li>
+    <li class="dropdown"><a href="#" class="trigger-drop">Edit Session<i class="arrow"></i></a>
+      <ul class="drop">
+        <li><a href="#">Add</a></li>
+        <li><a href="#">Delete</a></li>
+        <li><a href="#">Update</a></li>
+      </ul>
+    </li>
+</ul>
+<ul class ="links">
+    <li><a href="#">Log Out</a>
+    </li>
+  </ul>
+</nav>
+
+
+</header>
+
+<div align= justify>
+<form action   method = "POST">
+  SI ID:<br>
+  <input type = "text" name = "ID"><br>
+  SI Name:<br>
+  <input type = "text" name = "name"><br>
+  SI Password:<br> 
+  <input type = "text" name = "password"><br>
+  SI Email:<br>
+  <input type = "text" name = "email"><br>
+  
+  <input type = "submit" name = "Submit">
+</form>
+
+</div>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Roboto:300,400&display=swap" rel="stylesheet"> 	
+	<link rel ="stylesheet" href="stylemenu.css">
+	<link rel ="stylesheet" href="stylesheet.css">
+	<link rel ="stylesheet" href="stylesearch.css">
+	<script language=javascript>
+		function submitPostLink()
+		{
+		document.postlink.submit();
+		}
+	</script>
+	<link rel="javascript" href="progress.js">
