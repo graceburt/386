0276f901f09eff69266fa68532e4e4d18b1545f3
Steph W <swarman2@gulls.salisbury.edu>
2019-11-16 21:18:40 -0500

    restyled

--- add_si.php
+++ add_si.php
@@ -1,3 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Roboto:300,400&display=swap" rel="stylesheet"> 	
+	<link rel ="stylesheet" href="stylemenu.css">
+	<link rel ="stylesheet" href="stylesheet.css">
+	<script language=javascript>
+		function submitPostLink()
+		{
+		document.postlink.submit();
+		}
+	</script>
+	<link rel="javascript" href="progress.js">
+
+
+	<script language=javascript>
+		function submitPostLink()
+		{
+		document.postlink.submit();
+		}
+	</script>
+	<script language =javascript>
+		window.onbeforeunload = unsetsessionvar;
+		function unsetsessionvar()
+		{
+			unset($_SESSION['added_students']);
+		}
+		</script>
 <header>
 <?php
 session_start();
@@ -13,7 +47,7 @@ if (!isset($_SESSION['admin']))
 
 <nav id="navigation">
   <ul class="links" style = "float:left;">
-  <li><a href="#">Home</a></li>
+  <li><a href="admin_page.php">Home</a></li>
     <li class="dropdown"><a href="#" class="trigger-drop">Edit SI<i class="arrow"></i></a>
       <ul class="drop">
         <li><a href="add_si.php">Add</a></li>
@@ -37,36 +71,72 @@ if (!isset($_SESSION['admin']))
 
 
 </header>
+<body>
 
-<div align= justify>
-<form action   method = "POST">
-  SI ID:<br>
-  <input type = "text" name = "ID"><br>
-  SI Name:<br>
-  <input type = "text" name = "name"><br>
-  SI Password:<br> 
-  <input type = "text" name = "password"><br>
-  SI Email:<br>
-  <input type = "text" name = "email"><br>
-  
-  <input type = "submit" name = "Submit">
-</form>
+<div class = 'column' style = 'padding:0;float:none; width: 97%; margin-top: 5%; background-color:#dcdcdc'>
+<?php
+if (!isset($_SESSION['added_students']))
+{
+	$_SESSION['added_students']=array();
+}
 
+?>
+<div class='column' style = 'padding:0; text-align:center;float:left; width:30%;margin-left:15%; margin-right:0;height:75%;margin-top:7.5%; marign-bottom:7.5%;'>
+	<form action = "#" name = postlink method='post' style="padding-top:5%;">
+
+	<h4 style = 'margin:0; padding:0;padding-bottom:25px;'>Add SI Leader</h4>	
+          <input type="text" id = "id-input" name='id-input'
+                  rows = "1"
+		   style ='width:95%' placeholder = 'Student ID'></textarea>
+	<p></p>  
+	<input type="text" id = "dept" name='dept'
+                  rows = "1"
+                   style ='width:95%' placeholder = 'Department (ex COSC)'></textarea>
+	<p></p>  
+          <input type="text" id = "course" name='course'
+                  rows = "1"
+		  placeholder = 'Course Number (ex 386)' style ='width:95%'></textarea>
+	<p></p>  
+           <input type="text" id = "sec" name='sec'
+                  rows = "1"
+		   style ='width:95%' placeholder = 'Section Number (ex 001)'></textarea>
+	<p></p>
+	
+	<input type = "submit" value="Add SI"/>
+	</form>
 </div>
+<?php 
+if(strlen($_POST['id-input'])>0 && strlen($_POST['dept'])>0 && strlen($_POST['course'])>0 && strlen($_POST['sec'])>0)
+{
+	array_push($_SESSION['added_students'], array("id"=>$_POST['id-input'], "dept"=> $_POST['dept'], "course"=>$_POST['course'], "sec" => $_POST['sec']));
+}
 
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link href="https://fonts.googleapis.com/css?family=Raleway:400,300,600,800,900" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Roboto:300,400&display=swap" rel="stylesheet"> 	
-	<link rel ="stylesheet" href="stylemenu.css">
-	<link rel ="stylesheet" href="stylesheet.css">
-	<link rel ="stylesheet" href="stylesearch.css">
-	<script language=javascript>
-		function submitPostLink()
+if (count($_SESSION['added_students'])>0)
+{
+echo"<div class='column' style = 'padding:0; text-align:center;float:right; width:30%;margin-right:15%; margin-left:0;height:75%;margin-top:7.5%; marign-bottom:7.5%;'><h4><center> Added Leaders</center></h4>";
+	foreach($_SESSION['added_students'] as $student)
+	{
+		foreach($student as $att)
 		{
-		document.postlink.submit();
+			echo $att . " ";
 		}
-	</script>
-	<link rel="javascript" href="progress.js">
+		echo"<br>";
+		
+	}
+	echo"</div>";
+}
+/*
+if (count($added_students)>0)
+{
+	echo"<div class = 'column' style = 'float:right; width = 30%;'></div>";
+$connection = @mysqli_connect('localhost','swarman2','swarman2','SalisburySIDB');
+	if($connection->connect_error) {
+		die('Failed to Connect: '.$connection->connect_error);
+	}
+		
+	$query = "select name,ID from Student where Student_ID = ID";
+ */
+?>
+
+</body>
+</html>
