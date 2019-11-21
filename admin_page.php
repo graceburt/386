04e644b95e1ad68d00c5182c0ee61a8310852832
Steph W <swarman2@gulls.salisbury.edu>
2019-11-21 16:54:49 -0500

    added search bar functionality

--- admin_page.php
+++ admin_page.php
@@ -27,6 +27,28 @@ if (!isset($_SESSION['admin']))
 		{
 		document.postlink.submit();
 		}
+
+
+// JavaScript code 
+function search_si() { 
+    let input = document.getElementById('searchbar').value 
+	    input=input.toLowerCase();
+
+    
+<?php
+echo"<div class = column>";
+	$connection = @mysqli_connect('localhost','swarman2','swarman2','SalisburySIDB');
+	if($connection->connect_error) {
+		die('Failed to Connect: '.$connection->connect_error);
+	}
+		
+	?>
+echo"</div>";
+} 
+
+
+
+
 	</script>
 	<link rel="javascript" href="progress.js">
 
@@ -105,9 +127,9 @@ if (!isset($_SESSION['admin']))
 
 
 <div id="cover" style = "align:left;margin-left:-15vw;">
-  <form method="get" action="">
+  <form method="POST" action="#">
     <div class="tb">
-      <div class="td"><input type="text" placeholder="Search" required></div>
+      <div class="td"><input id="searchbar"  type="text"  name = "search" placeholder="Search" required></div>
       <div class="td" id="s-cover">
         <button class = "searchbutton" type="submit">
           <div id="s-circle"></div>
@@ -118,32 +140,17 @@ if (!isset($_SESSION['admin']))
   </form>
 </div>
 
-
-
-
-<!--
-<div class="row">
-  <div class="text_column">
-	<table>
-		<thead>
-			<tr>
-				<th align = 'center'>SI Leader</th>
-				<th align = 'center'>ATTENDANCE<br /> REPORTED</th> 
-				<th align = 'center'> OFFICE HOURS</th>
-			</tr>
-		</thead>
-	</table>
-</div>
--->
-</div>
-<div class = 'column' >
-     <?php
+<?php 
+	if(isset($_POST['search']))
+	{
+	echo "<div class = 'column' style = 'float:left; width:100%; height:400px;'>";
 	$connection = @mysqli_connect('localhost','swarman2','swarman2','SalisburySIDB');
 	if($connection->connect_error) {
 		die('Failed to Connect: '.$connection->connect_error);
 	}
 		
-	$query = "select name,ID from Student,Supplemental_Instruction_Leader where Student_ID = ID";
+	$query = "select name,ID from Student,Supplemental_Instruction_Leader where Student_ID = ID and name like '%".$_POST['search']."%'";
+	
 	echo"<table>
 		<thead>
 			<tr>
@@ -225,27 +232,115 @@ if (!isset($_SESSION['admin']))
 		}
 	}
 	echo"</table>";
-	/*
+		echo"</div>";
+	}
+?>
+
+<!--
+<div class="row">
+  <div class="text_column">
+	<table>
+		<thead>
+			<tr>
+				<th align = 'center'>SI Leader</th>
+				<th align = 'center'>ATTENDANCE<br /> REPORTED</th> 
+				<th align = 'center'> OFFICE HOURS</th>
+			</tr>
+		</thead>
+	</table>
+</div>
+-->
+</div>
+<div class = 'column' >
+     <?php
+	$connection = @mysqli_connect('localhost','swarman2','swarman2','SalisburySIDB');
+	if($connection->connect_error) {
+		die('Failed to Connect: '.$connection->connect_error);
+	}
+		
 	$query = "select name,ID from Student,Supplemental_Instruction_Leader where Student_ID = ID";
-	$r = mysqli_query($connection, $query);
+
 	echo"<table>
 		<thead>
 			<tr>
-				<th>SIL</th>
-				<th style = 'float:right;'>OFFICE HOURS</th> 
+			<th align = 'center'>SI Leader</th>
+			<th align = 'center' >ATTENDANCE <br/> REPORTED</th>
+			<th align = 'center' >OFFICE HOURS</th>
 			</tr>
 		</thead>";
+	
+	$r = mysqli_query($connection, $query);
 	while($row = mysqli_fetch_array($r))
 	{
-		$office_hours_query = "select start_time-end_time from Office_Hours where SI_ID = ".$row['ID'];
-		$sub_r = mysqli_query($connection, $office_hours_query);		
+		$attendance_query = "select session_date, DAYOFWEEK(session_date) as weekday, session_time, count(*) from Attends where SI_ID =".$row['ID']." group by session_date, session_time, SI_ID";
+		$sub_r = mysqli_query($connection, $attendance_query);		
+		$office_hours_query ="select HOUR(SEC_TO_TIME(sum(TIME_TO_SEC(TIMEDIFF(end_time,start_time)))))as hours, MINUTE(SEC_TO_TIME(sum(TIME_TO_SEC(TIMEDIFF(end_time,start_TIME))))) as minutes from Office_Hours where SI_ID =".$row['ID'];
+		$office_hr_r = mysqli_query($connection, $office_hours_query);
+		$office_hr = mysqli_fetch_array($office_hr_r);		
 		echo"<tr>";
-		echo"<td class = sql_text>".$row['name']."</td> <td style = 'float:right;'>".$sub_r['start_time-end_time'];
+		echo"<td style = 'padding-left:5%;'><strong>".$row['name']."</strong></td><td align ='center'> </td><td align = 'center'>";
+		if( $office_hr['hours'] >=2)
+		{
+			echo " FULFILLED ";
+		}
+		elseif(isset($office_hr['hours']))
+		{
+			echo "<div style = 'color:#e50000;'>".$office_hr['hours']." hrs  ".$office_hr['minutes']." mins </div>";
+
+		}
+		else
+		{
+			echo"<div style = 'color:#e50000;'> 0 hrs  0 mins </div>";
+		}
+		echo"</td>";
+		echo"</tr>";		
+
+		while($sub_row=mysqli_fetch_array($sub_r))
+		{
+			echo"<form action = 'session_page.php' name=postlink method = 'post'>
+				<input type = 'hidden' name='s-date' value=".$sub_row['session_date'].">
+			<input type = 'hidden' name='si-name' value=".$row['name'].">
+				<input type = 'hidden' name='s-time' value=".$sub_row['session_time'].">";
+			echo"<tr><td class='indent'><a href=# onclick='submitPostLink()'>";
+			if ($sub_row['weekday'] == 1)
+			{
+				echo"Sunday";
+			}
+			elseif ($sub_row['weekday'] == 2)
+			{
+				echo"Monday";
+			}
+			elseif ($sub_row['weekday'] == 3)
+			{
+				echo"Tuesday";
+			}
+			elseif ($sub_row['weekday'] == 4)
+			{
+				echo"Wednesday";
+			}
+			elseif ($sub_row['weekday'] == 5)
+			{
+				echo"Thurdsay";
+			}
+			elseif ($sub_row['weekday'] == 6)
+			{
+				echo"Friday";
+			}
+			elseif ($sub_row['weekday'] == 7)
+			{
+				echo"Saturday";
+			}
+			else
+			{
+				echo $sub_row['session-date'];
+			}
+			echo"</a></td> <td align = 'center'>".$sub_row['count(*)']."</td><td>";
+
 			echo"</td>";
 			echo"</tr>";
+		}
 	}
 	echo"</table>";
-	 */
      ?>
   </div>
 
