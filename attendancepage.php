dcddbc600edcf33337ac8227f5df00a0251370c4
Grace Burton <gburton5@gulls.salisbury.edu>
2019-12-12 21:28:03 -0500

    added dropdown

--- attendancepage.php
+++ attendancepage.php
@@ -91,9 +91,9 @@ $email = $_SESSION['uname']
     <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
     <span class="caret"></span></button>
     <ul class="dropdown-menu">
-      <li><a href="#">HTML</a></li>
-      <li><a href="#">CSS</a></li>
-      <li><a href="#">JavaScript</a></li>
+      <li><a href="#">EX</a></li>
+      <li><a href="#">EX</a></li>
+      <li><a href="#">EX</a></li>
     </ul>
   </div>
 
